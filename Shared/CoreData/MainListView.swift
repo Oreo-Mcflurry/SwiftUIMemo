551e18a4a0902a751ed5c681714b77260300d393
Musk <yuiop1029@naver.com>
2023-04-09 12:16:18 +0900

    Step 1

---
+++ Shared/CoreData/MainListView.swift
@@ -0,0 +1,20 @@
+//
+//  MainListView.swift
+//  SwiftUIMemo
+//
+//  Created by 유인호 on 2023/04/09.
+//
+
+import SwiftUI
+
+struct MainListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainListView()
+    }
+}
